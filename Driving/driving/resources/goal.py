1dafd65b5b4dd4d9afed3c72c17efbf8f8e53186
jcob-sikorski <jm.siekiera@gmail.com>
2023-05-18 18:00:54 +0200

    Added documentation.

--- Driving/driving/resources/goal.py
+++ Driving/driving/resources/goal.py
@@ -1,10 +1,21 @@
 import pybullet as p
 import os
-import numpy as np
-
 
 class Goal:
+    """
+    Class defining the Goal in the simulation environment.
+    """
+
     def __init__(self, client, base):
+        """
+        Initializes the Goal object.
+
+        Parameters:
+        - client: the id of the physics client,
+        - base: the position of the goal.
+        """
+        
+        # Load the URDF model of the goal
         f_name = os.path.join(os.path.dirname(__file__), 'goal.urdf')
         p.loadURDF(fileName=f_name,
                    basePosition=[base[0], base[1], 0],
